a6a4e2afd0fc623fac32f4f10afb5b30789f49d6
Jakub Gaik <jakub.gaik@gmail.com>
2025-03-26 13:31:55 +0100

    Update config and add function

--- src/utils/objects.ts
+++ src/utils/objects.ts
@@ -5,3 +5,9 @@ export function getTypedObjectKeys<T extends object>(obj: T) {
 export function getTypedObjectEntries<T extends object>(obj: T) {
   return Object.entries(obj) as [keyof T, T[keyof T]][];
 }
+
+export function getObjectFromFormData<T = Record<string, string>>(
+  formData: FormData
+) {
+  return Object.fromEntries(formData.entries()) as T;
+}
